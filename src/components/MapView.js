a44ea636071cf0840a06a1a11f5eaf75b4f186fe
ogawa-4 <yonrog163@gmail.com>
2025-06-17 15:23:33 +0900

    改良バｎ

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -5,7 +5,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 function MapView({ letters, currentPosition }) {
   return (
     <MapContainer
-      center={[35.6895, 139.6917]}
+      center={[35.6895, 139.6917]} // 東京駅あたり
       zoom={13}
       style={{ height: '400px', width: '100%', marginTop: '2rem' }}
     >
@@ -13,15 +13,24 @@ function MapView({ letters, currentPosition }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
       />
-      {letters.map((letter) => (
-        <Marker key={letter.id} position={[letter.latitude, letter.longitude]}>
-          <Popup>
-            {letter.content
-              ? letter.content
-              : "この手紙を読むには、もっと近くまで歩いてこなきゃね。"}
-          </Popup>
-        </Marker>
-      ))}
+
+      {/* 100m以内の手紙だけマップに表示 */}
+      {letters
+        .filter((letter) => letter.distance != null && letter.distance <= 100)
+        .map((letter) => (
+          <Marker
+            key={letter.id}
+            position={[letter.latitude, letter.longitude]}
+          >
+            <Popup>
+              {letter.content
+                ? letter.content
+                : "この手紙を読むには、もっと近くまで歩いてね。"}
+            </Popup>
+          </Marker>
+        ))}
+
+      {/* 現在地マーカー */}
       {currentPosition && (
         <Marker position={currentPosition}>
           <Popup>あなたの現在地</Popup>
